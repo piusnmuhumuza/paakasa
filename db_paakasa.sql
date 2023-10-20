8a733a4dcc7242dce6d6fde9706bc056888aec2b
piusmwilson <piusmnwilson@gmail.com>
2023-10-20 19:04:35 +0300

    --corrections

--- db_paakasa.sql
+++ db_paakasa.sql
@@ -13,6 +13,9 @@ SHOW DATABASES;
 -- to make the db_paakasa database the active database 
 USE db_paakasa;
 
+-- To drop the table if it exists
+DROP TABLE IF EXISTS TblPaakasa;
+
 -- TO create the table for the paakasa data
 CREATE TABLE TblPaakasa (
     orderID INT,
@@ -37,7 +40,6 @@ CREATE TABLE TblPaakasa (
     supplierID INT,
     categoryID INT,
     quantityPerUnit VARCHAR(255),
-    unitPrice_1 DECIMAL(10,2),
     product_unitPrice DECIMAL(10,2),
     unitsInStock INT,
     unitsOnOrder INT,
@@ -51,7 +53,7 @@ CREATE TABLE TblPaakasa (
 
 
 -- INSERTING DATA INTO THE CREATED TABLE
-LOAD DATA LOCAL INFILE 'G:\projects\paakasa\paakasa.csv'-- table path
+/**LOAD DATA LOCAL INFILE 'G:\projects\paakasa\paakasa.csv'-- table path
 INTO TABLE TblPaakasa
 FIELDS TERMINATED BY ',' -- for a csv file
 ENCLOSED BY '"' -- for the strings
@@ -61,6 +63,11 @@ IGNORE 1 ROWS; -- ignore the headers
 -- to check the table for the loaded data
 SELECT *
 FROM TblPaakasa;
+/////
+This works if you have access enabled on the User and clients side. 
+Due to issues with getting access so I used the Table Import Wizard and the table was successfully
+imported into the db_paakasa database created on MySQL Workbench.
+**/
 
 
 /** Normalization and Denormalization **/
@@ -160,9 +167,9 @@ FROM orders;
 -- TO CREATE THE EMPLOYEES TABLE
 CREATE TABLE IF NOT EXISTS employees AS 
 SELECT DISTINCT employeeID,
-    employee_firstname,
-    employee_lastname,
-    CONCAT(employee_firstname, ' ', employee_lastname) AS full_name,
+    employees_lastName,
+    employees_firstName,
+    CONCAT(employees_firstName, ' ', employees_lastName) AS full_name,
     employees_title
 FROM TblPaakasa
 ORDER BY employeeID;
@@ -170,3 +177,143 @@ ORDER BY employeeID;
 -- To check if the table was created and the data was loaded
 SELECT *
 FROM employees;
+
+
+
+
+/** Database Diagram Design and Table Alterations **/
+-- ALTER TABLE CONSTRAINTS
+
+
+
+-- FOR THE CATEGORIES TABLE
+DESCRIBE categories;
+
+-- From observing the table, the category ID should be a Primary key (NOT NULL & UNIQUE) and AUTO INCREMENT. 
+-- The CategoryName column should be not be null and it should be Unique. The datatype should be VARCHAR(255)
+-- To Alter the table constraints below
+
+ALTER TABLE categories
+MODIFY categoryID INT AUTO_INCREMENT PRIMARY KEY, -- For Category ID
+MODIFY categoryName VARCHAR(255) NOT NULL UNIQUE; -- For Category Name
+
+-- FOR THE CUSTOMERS TABLE
+DESCRIBE customers;
+
+/* 
+From observing the table, the customerID is the Primary Key. 
+The Company Name, Contact Name and Contact Title
+column should not be null. 
+*/
+
+ALTER TABLE customers 
+CHANGE COLUMN customerID customerID CHAR(5) NOT NULL ,
+CHANGE COLUMN companyName companyName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN contactName contactName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN contactTitle contactTitle VARCHAR(255) NOT NULL ,
+ADD PRIMARY KEY (customerID);
+;
+
+-- FOR THE SUPPLIERS TABLE
+DESCRIBE suppliers;
+
+/* 
+From observing the columns in the table, the supplierID is the Primary Key and AUTO INCREMENT. The companyname, contactname 
+and contactTitle should not be null
+*/
+
+ALTER TABLE suppliers 
+CHANGE COLUMN supplierID supplierID INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN companyName companyName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN contactName contactName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN contactTitle contactTitle VARCHAR(255) NOT NULL ,
+ADD PRIMARY KEY (supplierID),
+ADD UNIQUE INDEX supplierID_UNIQUE (supplierID ASC) VISIBLE;
+;
+
+
+-- FOR THE EMPLOYEES TABLE
+DESCRIBE employees;
+
+/* 
+From observing the table, the Employee ID is the Primary Key & Auto Increment then other columns are Not Null and the Data type
+would be changed to VARCHAR(255)
+*/
+
+ALTER TABLE employees
+CHANGE COLUMN employeeID employeeID INT NOT NULL AUTO_INCREMENT ,
+CHANGE COLUMN employees_lastName employees_lastName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN employees_firstName employees_firstName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN full_name full_name VARCHAR(255) NOT NULL ,
+CHANGE COLUMN employees_title employees_title VARCHAR(255) NOT NULL ,
+ADD PRIMARY KEY (employeeID);
+;
+
+
+-- FOR THE PRODUCTS TABLE
+DESCRIBE products;
+
+/* 
+From my Observation, Product ID is the Primary Key, Category ID and Supplier ID are Foreign keys. Discontinued is a Boolean 
+so it would be TinyINT since MySQL doesnt support Boolean. The Unit Price should be a Decimal Data Type. 
+*/ 
+
+ALTER TABLE products 
+CHANGE COLUMN productID productID INT NOT NULL ,
+CHANGE COLUMN productName productName VARCHAR(255) NOT NULL ,
+CHANGE COLUMN quantityPerUnit quantityPerUnit VARCHAR(255) NULL DEFAULT NULL ,
+CHANGE COLUMN product_unitPrice product_unitPrice DECIMAL(10,2) NULL DEFAULT NULL ,
+CHANGE COLUMN discontinued discontinued TINYINT NULL DEFAULT NULL ,
+ADD PRIMARY KEY (productID),
+ADD UNIQUE INDEX productName_UNIQUE (productName ASC) VISIBLE,
+ADD INDEX Supplier_fk_idx (supplierID ASC) VISIBLE,
+ADD INDEX Category_fk_idx (categoryID ASC) VISIBLE;
+;
+
+-- To Include the Foreign key for Supplier ID and Category ID
+ALTER TABLE products 
+ADD CONSTRAINT Supplier_fk
+    FOREIGN KEY (supplierID)
+    REFERENCES db_northwind.suppliers (supplierID)
+    ON DELETE CASCADE 
+    ON UPDATE RESTRICT,
+ADD CONSTRAINT Category_fk
+    FOREIGN KEY (categoryID)
+    REFERENCES db_northwind.categories (categoryID)
+    ON DELETE CASCADE 
+    ON UPDATE RESTRICT;
+
+
+-- FOR THE ORDERS TABLE
+DESCRIBE orders;
+
+/* AFter observing the tabel, the following constraints were added to the table */
+
+ALTER TABLE orders 
+CHANGE COLUMN orderID orderID INT NOT NULL ,
+CHANGE COLUMN customerID customerID CHAR(5) NULL DEFAULT NULL ,
+CHANGE COLUMN productID productID INT NOT NULL ,
+CHANGE COLUMN unitPrice unitPrice DOUBLE NOT NULL ,
+CHANGE COLUMN quantity quantity INT NOT NULL ,
+ADD INDEX employee_fk_idx (employeeID ASC) VISIBLE,
+ADD INDEX customer_fk_idx (customerID ASC) VISIBLE,
+ADD INDEX product_fk_idx (productID ASC) VISIBLE;
+
+
+-- To add the foreign key to the tables
+ALTER TABLE db_TblPaakasa.orders 
+ADD CONSTRAINT employee_fk
+  FOREIGN KEY (employeeID)
+  REFERENCES db_TblPaakasa.employees (employeeID)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT customer_fk
+  FOREIGN KEY (customerID)
+  REFERENCES db_TblPaakasa.customers (customerID)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT product_fk
+  FOREIGN KEY (productID)
+  REFERENCES db_TblPaakasa.products (productID)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
