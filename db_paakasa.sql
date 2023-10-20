f0014ca7682ab5e391c8c3af5be1d6dfb4e1550b
piusmwilson <piusmnwilson@gmail.com>
2023-10-20 18:17:50 +0300

    Data Normalization

--- db_paakasa.sql
+++ db_paakasa.sql
@@ -1,3 +1,6 @@
+
+/** Database Creation & Data Loading **/
+
 -- Dropping the database if it exists 
 DROP DATABASE IF EXISTS db_paakasa;
 
@@ -34,6 +37,7 @@ CREATE TABLE TblPaakasa (
     supplierID INT,
     categoryID INT,
     quantityPerUnit VARCHAR(255),
+    unitPrice_1 DECIMAL(10,2),
     product_unitPrice DECIMAL(10,2),
     unitsInStock INT,
     unitsOnOrder INT,
@@ -47,8 +51,7 @@ CREATE TABLE TblPaakasa (
 
 
 -- INSERTING DATA INTO THE CREATED TABLE
-LOAD DATA LOCAL INFILE 'G:\projects\paakasa\paakasa.csv'
-          -- table path
+LOAD DATA LOCAL INFILE 'G:\projects\paakasa\paakasa.csv'-- table path
 INTO TABLE TblPaakasa
 FIELDS TERMINATED BY ',' -- for a csv file
 ENCLOSED BY '"' -- for the strings
@@ -59,3 +62,111 @@ IGNORE 1 ROWS; -- ignore the headers
 SELECT *
 FROM TblPaakasa;
 
+
+/** Normalization and Denormalization **/
+/***
+After Normalizing the table, The table was sub-divided into the following entities
+-- Customers
+-- Categories
+-- Suppliers
+-- Products
+-- Orders
+-- Employees
+ ***/
+
+-- To get the column names of the dataset
+DESCRIBE TblPaakasa;
+
+-- TO CREATE THE CUSTOMER TABLE
+CREATE TABLE IF NOT EXISTS customers AS
+SELECT DISTINCT customerID,
+    companyName,
+    contactName,
+    contactTitle       
+FROM TblPaakasa;
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM customers;
+
+-- TO CREATE THE CATEGORIES TABLE
+CREATE TABLE IF NOT EXISTS categories AS 
+SELECT DISTINCT categoryID,
+    categoryName
+FROM TblPaakasa
+ORDER BY categoryID;
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM categories;
+
+-- TO CREATE THE SUPPLIERS TABLE
+CREATE TABLE IF NOT EXISTS suppliers AS
+SELECT DISTINCT supplierID,
+	supplier_companyName AS companyName,
+    supplier_contactName AS contactName,
+    supplier_contactTitle AS contactTitle
+FROM TblPaakasa
+ORDER BY supplierID;
+
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM suppliers;
+
+
+-- TO CREATE THE PRODUCTS TABLE
+CREATE TABLE IF NOT EXISTS products AS 
+SELECT DISTINCT productID,
+    productName,
+    categoryID,
+    supplierID,
+    quantityPerUnit,
+    product_unitPrice,
+    unitsInStock,
+	unitsOnOrder,
+	reorderLevel,
+	discontinued
+FROM TblPaakasa
+ORDER BY productID;
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM products;
+
+
+-- TO CREATE THE ORDERS TABLE
+CREATE TABLE IF NOT EXISTS orders AS 
+SELECT  orderID,
+    customerID,
+    employeeID,
+    STR_TO_DATE(orderDate, '%m/%d/%Y') orderDate,
+    STR_TO_DATE(requiredDate, '%m/%d/%Y') requiredDate,
+    STR_TO_DATE(shippedDate, '%m/%d/%Y') shippedDate,
+    shipVia,
+    Freight,
+    productID,
+    unitPrice,
+    quantity,
+    discount
+FROM TblPaakasa
+ORDER BY orderID;
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM orders;
+
+
+-- TO CREATE THE EMPLOYEES TABLE
+CREATE TABLE IF NOT EXISTS employees AS 
+SELECT DISTINCT employeeID,
+    employee_firstname,
+    employee_lastname,
+    CONCAT(employee_firstname, ' ', employee_lastname) AS full_name,
+    employees_title
+FROM TblPaakasa
+ORDER BY employeeID;
+
+-- To check if the table was created and the data was loaded
+SELECT *
+FROM employees;
